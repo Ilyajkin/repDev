0ce5a606d288d9d9e67501be2b0a9cc34b0018c1
Ilya Marchenkov <mar4enkov@inbox.ru>
2020-06-08 18:19:23 +0400

    Add paths

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-require_once 'application/bootstrap.php';
 
+require_once 'core/Router.php';
+require_once 'core/Route.php';
 
+$router = new Router();
+$route = $router->getRoute();
 
+$controllerName = $route->getControllerName();
+$modelName = $route->getModelName();
 
-
+echo $controllerName . PHP_EOL;
+echo $modelName . PHP_EOL;
